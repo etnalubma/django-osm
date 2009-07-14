94c1eefd12bc4c0ac1c9271e92543fa7924b1c63
etnalubma <francisco.herrero@gmail.com>
2009-07-13 22:09:19 -0300

    first commit

---
+++ osm/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Add your admin site registrations here, eg.
+# from osm.models import Author
+# admin.site.register(Author)
---
+++ osm/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+# URL patterns for osm
+
+urlpatterns = patterns('osm.views',
+  # Add url patterns here
+)
